11bafc2a1880f0333c1c91d1ca06c68858280d0e
Peter Cheung <pcheung@wayfair.com>
2017-09-15 10:22:23 -0400

    Removed logic that caused edgeInsets to update incorrectly

--- Source/Cells/BrickCell.swift
+++ Source/Cells/BrickCell.swift
@@ -181,17 +181,12 @@ open class BrickCell: BaseBrickCell {
         return UIEdgeInsetsMake(defaultTopConstraintConstant, defaultLeftConstraintConstant, defaultBottomConstraintConstant, defaultRightConstraintConstant)
     }
 
-    private var didUpdateEdgeInsets: Bool = false
     @objc open dynamic var edgeInsets: UIEdgeInsets = UIEdgeInsets.zero {
         didSet {
-            if edgeInsets == oldValue {
-                return
-            }
             self.topSpaceConstraint?.constant = edgeInsets.top
             self.bottomSpaceConstraint?.constant = edgeInsets.bottom
             self.leftSpaceConstraint?.constant = edgeInsets.left
             self.rightSpaceConstraint?.constant = edgeInsets.right
-            didUpdateEdgeInsets = true
         }
     }
 
@@ -231,13 +226,6 @@ open class BrickCell: BaseBrickCell {
         guard self._brick.height.isEstimate(withValue: nil) else {
             return layoutAttributes
         }
-
-        if !didUpdateEdgeInsets {
-            guard let brickAttributes = layoutAttributes as? BrickLayoutAttributes, brickAttributes.isEstimateSize else {
-                return layoutAttributes
-            }
-        }
-        didUpdateEdgeInsets = false
 
         let preferred = layoutAttributes
 
